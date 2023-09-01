3abf6c0639aca8401bbe35d84be70e81aafd240d
perronivitor <vitorope48@gmail.com>
2023-08-31 21:34:44 -0300

    mapeamento dos obejtos de response da api da marvel

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,3 +7,16 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
 }
+
+dependencies {
+
+    // Retrofit
+    val retrofitVersion = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    //Gson
+    val gsonVersion = "2.9.0"
+    implementation("com.google.code.gson:gson:$gsonVersion")
+
+}
